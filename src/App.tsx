38becdc5b613ea5039deef1454ae5cfd3b1f4a8e
Phung Anh Khoa <thiengoten6@gmail.com>
2024-11-11 23:01:50 +0700

    feat: setup auth api and change code in theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { Spinner } from "@/components/ui/spinner";
-import { RootContainer as router } from "@/containers";
-import { QueryProvider } from "@/providers";
-import { RouterProvider } from "react-router-dom";
-import { ThemeProvider } from "./components/them-provider";
+import { Spinner } from '@/components/ui/spinner'
+import { RootContainer as router } from '@/containers'
+import { QueryProvider, ThemeProvider } from '@/providers'
+import { RouterProvider } from 'react-router-dom'
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
       <QueryProvider>
         <RouterProvider router={router} fallbackElement={<Spinner />} />
       </QueryProvider>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/queries/signup/signup.types.ts
@@ -0,0 +1,4 @@
+export type SignupPayload = {
+  email: string
+  password: string
+}
