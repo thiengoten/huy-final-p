74691be676532170708123ce2b57666a6a2c5811
Phung Anh Khoa <thiengoten6@gmail.com>
2024-11-11 23:01:50 +0700

    feat: setup auth api and change code in theme provider

---
+++ src/api/auths/api.ts
@@ -0,0 +1,9 @@
+import { supabase,  } from "@/services"
+
+export const signUp = async (email: string, password: string) => {
+  return await supabase.auth.signUp({ email, password })
+}
+
+export const signIn = async (email: string, password: string) => {
+  return await supabase.auth.signInWithPassword({ email, password })
+}
