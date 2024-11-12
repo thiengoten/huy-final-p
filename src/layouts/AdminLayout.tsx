f8fdbec19367cfac69684c535b88c8f045b703be
Phung Anh Khoa <thiengoten6@gmail.com>
2024-11-12 10:34:14 +0700

    fix: add format code and update code

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,9 +1,9 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom"
 
 const AdminLayout = () => {
   return (
     <>
-      <main>
+      <main className="bg-loginBackground">
         <Outlet />
       </main>
     </>
--- src/modules/auth/view/LoginView.tsx
+++ src/modules/auth/view/LoginView.tsx
@@ -1,12 +1,12 @@
-import LoginForm from "@/components/login-form";
+import LoginForm from "@/components/login-form"
 
 //TODO: Huy will implement here
 const Login = () => {
   return (
-    <div className="container h-[100vh] place-content-center bg-loginBackground ">
+    <div className="container h-[100vh] place-content-center">
       <LoginForm />
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
