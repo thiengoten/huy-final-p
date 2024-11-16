09119ca7cf1db6e3a6d37f041dc8885e5d7f1c7e
danghuy6584 <84719301+danghuy6584@users.noreply.github.com>
2024-11-16 23:48:48 +0700

    Feature/kho 31 intergate api for registerpage (#40)
    * feat : intergrate register

---
+++ src/components/Auth/register-form.helper.ts
@@ -0,0 +1,23 @@
+import { z } from "zod"
+
+export const SignUpFormSchema = z
+  .object({
+    email: z.string().email({ message: "Invalid email address" }),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters" }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  })
+
+// Initial values for the form
+export const signupInitValues = {
+  email: "",
+  password: "",
+  confirmPassword: "",
+}
