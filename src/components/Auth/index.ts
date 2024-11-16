4a66ed2d57be5876164ca2c85788edc40446453b
Van Huy <danghuy6584@gmail.com>
2024-11-16 17:22:42 +0700

    feat : intergrate register

--- src/components/Auth/index.ts
+++ src/components/Auth/index.ts
@@ -1,2 +1,4 @@
 export { default as LoginForm } from "./login-form"
+export * from "./login-form.helpers"
 export { default as RegisterForm } from "./register-form"
+export * from "./register-form.helper"
--- src/components/Auth/register-form.tsx
+++ src/components/Auth/register-form.tsx
@@ -8,119 +8,181 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useSignup } from "@/queries/signup"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { Eye, EyeOff } from "lucide-react"
 import React from "react"
+import { useForm } from "react-hook-form"
 import { Link } from "react-router-dom"
+import { z } from "zod"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "../ui/form"
+import { SignUpFormSchema, signupInitValues } from "./register-form.helper"
 
 export default function Registerform() {
   const [showPassword, setShowPassword] = React.useState(false)
   const [showConfirmPassword, setShowConfirmPassword] = React.useState(false)
 
+  const { onSignupUser } = useSignup({
+    onSuccess: (data) => {
+      console.log("User logged in", data)
+    },
+    onError: (error) => {
+      console.error("Error logging in", error)
+    },
+  })
+
+  const form = useForm<z.infer<typeof SignUpFormSchema>>({
+    resolver: zodResolver(SignUpFormSchema),
+    defaultValues: signupInitValues,
+  })
+
+  const { control, handleSubmit } = form
+
+  const onSubmit = (values: z.infer<typeof SignUpFormSchema>) => {
+    onSignupUser(values)
+  }
+
   const togglePasswordVisibility = () => setShowPassword(!showPassword)
   const toggleConfirmPasswordVisibility = () =>
     setShowConfirmPassword(!showConfirmPassword)
   return (
-    <div className="flex h-[80vh] w-full items-center justify-center">
-      <Card className="flex w-full max-w-4xl overflow-hidden">
-        <div className="flex-1 p-6 flex flex-col items-stretch justify-center">
-          <CardHeader>
-            <CardTitle className="text-2xl">Create an Account</CardTitle>
-            <CardDescription>
-              Enter your details below to create your account
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form className="grid gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor="name">Full Name</Label>
-                <Input id="name" type="text" placeholder="John Doe" required />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="password">Password</Label>
-                <div className="relative">
-                  <Input
-                    id="password"
-                    type={showPassword ? "text" : "password"}
-                    required
-                  />
-                  <Button
-                    type="button"
-                    variant="ghost"
-                    size="icon"
-                    className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
-                    onClick={togglePasswordVisibility}
-                    aria-label={
-                      showPassword ? "Hide password" : "Show password"
-                    }
-                  >
-                    {showPassword ? (
-                      <EyeOff className="h-4 w-4" />
-                    ) : (
-                      <Eye className="h-4 w-4" />
+    <Form {...form}>
+      <div className="flex h-[80vh] w-full items-center justify-center">
+        <Card className="flex w-full max-w-4xl overflow-hidden">
+          <div className="flex-1 p-6 flex flex-col items-stretch justify-center">
+            <CardHeader>
+              <CardTitle className="text-2xl">Create an Account</CardTitle>
+              <CardDescription>
+                Enter your details below to create your account
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form className="grid gap-4" onSubmit={handleSubmit(onSubmit)}>
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <FormField
+                    control={control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            placeholder="m@example.com"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
                     )}
-                  </Button>
-                </div>
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="confirm-password">Confirm Password</Label>
-                <div className="relative">
-                  <Input
-                    id="confirm-password"
-                    type={showConfirmPassword ? "text" : "password"}
-                    required
                   />
-                  <Button
-                    type="button"
-                    variant="ghost"
-                    size="icon"
-                    className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
-                    onClick={toggleConfirmPasswordVisibility}
-                    aria-label={
-                      showConfirmPassword ? "Hide password" : "Show password"
-                    }
-                  >
-                    {showConfirmPassword ? (
-                      <EyeOff className="h-4 w-4" />
-                    ) : (
-                      <Eye className="h-4 w-4" />
-                    )}
-                  </Button>
                 </div>
-              </div>
-              <Button type="submit" className="w-full">
-                Create Account
-              </Button>
-              <Button variant="outline" className="w-full">
-                Sign up with Google
-              </Button>
-              <div className="mt-4 text-center text-sm">
-                Already have an account?{" "}
-                <Link to="../login" className="underline">
-                  Log in
-                </Link>
-              </div>
-            </form>
-          </CardContent>
-        </div>
-        <div className="hidden flex-1 md:block">
-          <img
-            src="https://i.pinimg.com/736x/fe/37/3e/fe373e1d755f8f596ce33829b764e6c0.jpg"
-            alt="Login image"
-            width={600}
-            height={600}
-            className="h-full w-full object-cover"
-          />
-        </div>
-      </Card>
-    </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="password">Password</Label>
+                  <div className="relative">
+                    <FormField
+                      control={control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <div className="relative">
+                              <Input
+                                type={showPassword ? "text" : "password"}
+                                {...field}
+                              />
+                              <Button
+                                type="button"
+                                variant="ghost"
+                                size="icon"
+                                className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+                                onClick={togglePasswordVisibility}
+                                aria-label={
+                                  showPassword
+                                    ? "Hide password"
+                                    : "Show password"
+                                }
+                              >
+                                {showPassword ? (
+                                  <EyeOff className="h-4 w-4" />
+                                ) : (
+                                  <Eye className="h-4 w-4" />
+                                )}
+                              </Button>
+                            </div>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="confirm-password">Confirm Password</Label>
+                  <div className="relative">
+                    <FormField
+                      control={control}
+                      name="confirmPassword"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <div className="relative">
+                              <Input
+                                type={showPassword ? "text" : "password"}
+                                {...field}
+                              />
+                              <Button
+                                type="button"
+                                variant="ghost"
+                                size="icon"
+                                className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+                                onClick={toggleConfirmPasswordVisibility}
+                                aria-label={
+                                  showPassword
+                                    ? "Hide password"
+                                    : "Show password"
+                                }
+                              >
+                                {showPassword ? (
+                                  <EyeOff className="h-4 w-4" />
+                                ) : (
+                                  <Eye className="h-4 w-4" />
+                                )}
+                              </Button>
+                            </div>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </div>
+                <Button type="submit" className="w-full">
+                  Create Account
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Sign up with Google
+                </Button>
+                <div className="mt-4 text-center text-sm">
+                  Already have an account?{" "}
+                  <Link to="../login" className="underline">
+                    Log in
+                  </Link>
+                </div>
+              </form>
+            </CardContent>
+          </div>
+          <div className="hidden flex-1 md:block">
+            <img
+              src="https://i.pinimg.com/736x/fe/37/3e/fe373e1d755f8f596ce33829b764e6c0.jpg"
+              alt="Login image"
+              width={600}
+              height={600}
+              className="h-full w-full object-cover"
+            />
+          </div>
+        </Card>
+      </div>
+    </Form>
   )
 }
